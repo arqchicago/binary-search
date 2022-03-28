9638e62a4722eef2c1e05756b873e83bb1bc25c0
Ahmad Qadri <arqchicago@gmail.com>
2022-03-28 08:02:29 -0400

    demonstrated interpolation search

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
-# Binary Search
-This technique finds the position of a value in an array that has already been sorted. The input to the algorithm is an array and a 
-target value.
+# Search Algorithms
+This repository implements different search algorithms and compares their runtimes. 
 
-## Algorithm
+
+## Linear Search
+
+### Search Concept
+Linear search involves sequentially iterating over elements of an array, starting from the first element and comparing the target until 
+either the target is found or all elements have been traversed.  
+
+### Pseudocode
+The pseudocode implemented is as follows:
+
+	function linear_search(array, target):
+		for i in range(len(array)):
+			if target==array[i]:
+				return i
+
+		return -999
+
+
+### Complexity
+The complexity of this algorithm is O(n)
+	
+
+## Binary Search
+
+### Search Concept
 The function assumes that the input array is already sorted. It starts by pointing to the first and last element of the array and reading
 their index values (0, length(array)-1) into 'left' and 'right' variables. The left and right variables are then used to calculate the
 middle value index. The algorithm compares this middle value to the target and if it is the same, it returns the index. Otherwise,
@@ -11,25 +34,28 @@ the lower or upper half of the array. If, for example, the target value is great
 variable is updated to be the index of middle value. The middle index is calculated again and the process is repeated until either the 
 target is found or the target is deemed to not be in the array. 
 
-## Pseudocode
+### Pseudocode
 The pseudocode implemented is as follows:
-'''
-function binary_search(array, target):
-    left = 0
-    right = len(array)-1
-    i = 0
-    while(left<=right):
-        
-        mid = int(round((left+right)/2, 0))
-        
-        if target==array[mid]:
-            return mid
-            
-        elif target>array[mid]:
-            left = mid+1
-            
-        elif target<array[mid]:
-            right = mid-1
-    
-    return -999
-'''
+
+	function binary_search(array, target):
+		left = 0
+		right = len(array)-1
+		i = 0
+		while(left<=right):
+			
+			mid = int(round((left+right)/2, 0))
+			
+			if target==array[mid]:
+				return mid
+				
+			elif target>array[mid]:
+				left = mid+1
+				
+			elif target<array[mid]:
+				right = mid-1
+		
+		return -999
+
+
+### Complexity
+The complexity of this algorithm is O(log n)
--- sorted_searches.py
+++ sorted_searches.py
@@ -35,6 +35,39 @@ def binary_search(array, target):
     return -999
 
 
+def interpolation_search(array, target):
+    '''
+    interpolation_search function performs interpolation search to find a target in a sorted array
+    
+    
+    :param array:   a python list containing elements
+    :param target:  a target value to be found in the array
+    
+    :return:        index of target if the target is found in the array, otherwise -999
+    '''
+    
+    left = 0
+    right = len(array)-1
+    i = 0
+    while(left<=right):
+        
+        # caculate middle index of the array
+        mid = int(round(left + ((target - array[left]) * (right - left) / (array[right] - array[left])), 0))
+        
+        # if target equals middle value of the array, return items
+        # otheriwse, update left or right variables based on which half the target may be contained in
+        if target==array[mid]:
+            return mid
+            
+        elif target>array[mid]:
+            left = mid+1
+            
+        elif target<array[mid]:
+            right = mid-1
+    
+    return -999
+
+
 def linear_search(array, target):
     '''
     linear_search function performs linear search to find a target in a sorted array
@@ -57,17 +90,18 @@ def linear_search(array, target):
 if __name__ == "__main__":
     
     start_time = time.time()
-    array_= [i for i in range(200000000)]
-    target_ = 199999999
+    array_= [i for i in range(100000000)]
+    target_ = 99999999
     
     #array_= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
     #target_ = 2
     
     print(f'array created:  array length={len(array_)},  target={target_},  time={time.time()-start_time}')
 
+
+    # Linear Search
     start_time = time.time()
     idx = linear_search(array_, target_)
-
     
     if idx != -999:
         print(f'linear search:  index={idx},  value={array_[idx]},  target={target_},  time={time.time()-start_time}')
@@ -75,12 +109,24 @@ if __name__ == "__main__":
     else:
         print(f'linear search:  index={idx},  value=NOT FOUND,  target={target_},  time={time.time()-start_time}')
 
+
+    # Binary Search
     start_time = time.time()
     idx = binary_search(array_, target_)
-
     
     if idx != -999:
         print(f'binary search:  index={idx},  value={array_[idx]},  target={target_},  time={time.time()-start_time}')
     
     else:
         print(f'binary search:  index={idx},  value=NOT FOUND,  target={target_},  time={time.time()-start_time}')
+
+
+    # Interpolation Search
+    start_time = time.time()
+    idx = interpolation_search(array_, target_)
+    
+    if idx != -999:
+        print(f'interpolation search:  index={idx},  value={array_[idx]},  target={target_},  time={time.time()-start_time}')
+    
+    else:
+        print(f'interpolation search:  index={idx},  value=NOT FOUND,  target={target_},  time={time.time()-start_time}')
